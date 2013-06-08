21c93ca5cdbf002662b179ea1754936f4bf7693c
root <root@debian>
2013-06-08 21:00:44 +0200

    Added some more stuffs, now a binary will be created in path

--- setup.py
+++ setup.py
@@ -2,13 +2,18 @@
 from distutils.core import setup
 import os
 
+scripts=['printedQr']
 if os.name is not "posix":
     if os.name is "nt":
         import py2exe
+    shutil.copyfile('digenpy','digenpy.py')
+    shutil.copyfile('digenpy-gtk','digenpy-gtk.py')
+    shutil.copyfile('digenpy-gtk','digenpy-gtk.py')
+    scripts=['printedQr.py']
 
 opts = {
     "py2exe": {
-        'packages': ['printedQr'],
+        'packages': ['printedQr_'],
     }
 }
 
@@ -18,6 +23,8 @@ setup(
     description='Print qr codes in Stl or scad',
     author='David Francos Cuartero',
     author_email='me@davidfrancos.net',
+    windows = [{"script": "printedQr.py" }],
+    console = [{"script": "printedQr.py" }],
     url='http://github.com/dlabs-co/printedQr',
     download_url='http://github.com/dlabs-co/printedQr',
     license='GPL2',
@@ -25,11 +32,11 @@ setup(
     mantainer='David Francos Cuartero (XayOn)',
     mantainer_email='dfrancos@dlabs.co',
     long_description="Print qr codes in stl or scad",
-    packages=['printedQr'],
+    packages=['printedQr_'],
+    scripts=scripts,
     entry_points="""
         [console_scripts]
-        calentic = calentic.server:server
-        calentic_scrappery = calentic.scrappery:main
+        printedQr = printedQr.printedQr:execute
     """,
     options=opts,
 )
